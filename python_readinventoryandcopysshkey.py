542ad1c64fcf65d30da0065df671f1d546e02b15
root <root@ans01.wcbc.local>
2017-08-12 20:33:17 -0400

    script now fully fuctional. places specified ssh keys on all ansible inventory

--- python_readinventoryandcopysshkey.py
+++ python_readinventoryandcopysshkey.py
@@ -1,5 +1,13 @@
+import subprocess
 
+# ansible inventory .ini file
 filename = '/etc/ansible/hosts'
+# the public cert that will be placed into authorized_keys
+sshpubkey = '/root/.ssh/id_rsa.pub'
+# remote system local account
+svcacc = 'root'
+# password  for remote system local account
+sshpassword = ''
 
 #open our .ini file for reading
 f = open(filename, 'r')
@@ -25,5 +33,14 @@ for object in output:
 #removes all blank entries from final list (these are left from removing new lines above)
 output2 = filter(None, output2)
             
-print output2
+#at this point output2 is a list of hostnames or ip addresses and ready for use.
 
+#copy public ssh keys to systems in /etc/ansible/hosts
+for system in output2:
+    # we define the bash command this was vs directly on subprocess because subprocess cannot handle
+    # white space inside quotes. 
+    bashcmd = 'sshpass -p %s ssh-copy-id -i %s %s@%s' % ( sshpassword, sshpubkey, svcacc, system )
+    # intiating the copy of ssh keys to systems in the inventory
+    subprocess.call(['bash','-c', bashcmd])
+
+    
