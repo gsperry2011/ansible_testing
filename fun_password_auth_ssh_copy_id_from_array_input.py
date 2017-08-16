dc7d643f45c8a1f324fd4592b9dcd5fa98894002
Greg Perry <gsperry2011@gmail.com>
2017-08-16 19:30:15 -0400

    added output message when copying ssh keys

--- fun_password_auth_ssh_copy_id_from_array_input.py
+++ fun_password_auth_ssh_copy_id_from_array_input.py
@@ -6,7 +6,10 @@ def password_auth_ssh_copy_id_from_array_input( sshpassword , input_file='/etc/a
     input_array = parse_ini(input_file)
 
     for system in input_array:
-        # we define the bash command this was vs directly on subprocess because subprocess cannot handle
+        # we define the bash command this was vs directly on subprocess because subprocess cannot
+        # handle
+
+        print 'attempting to copy ssh key to %s' % system
         # white space inside quotes. 
         bashcmd = 'sshpass -p %s ssh-copy-id -i %s %s@%s' % ( sshpassword, sshpubkey, svcacc, system )
         # intiating the copy of ssh keys to systems in the inventory
