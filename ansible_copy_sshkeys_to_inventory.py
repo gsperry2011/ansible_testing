0b3c5bb10ae331630425f4e9f6369b50b3cc44a3
Greg Perry <gsperry2011@gmail.com>
2017-08-16 19:29:18 -0400

    untested

--- ansible_copy_sshkeys_to_inventory.py
+++ ansible_copy_sshkeys_to_inventory.py
@@ -1,3 +1,6 @@
+#run this script and pass your ssh password to copy the specified ssh keys to all systems in the
+#ansible inventory
+
 from fun_password_auth_ssh_copy_id_from_array_input import password_auth_ssh_copy_id_from_array_input
 
-password_auth_ssh_copy_id_from_array_input()
+password_auth_ssh_copy_id_from_array_input(svcacc=(sys.argv[1]) sshpassword=(sys.argv[2]), sshpubkey='/root/.ssh/id_rsa.pub')
