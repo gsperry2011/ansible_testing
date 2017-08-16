51de625789675bbb974d5928475ee6caf359c398
Greg Perry <gsperry2011@gmail.com>
2017-08-16 18:55:35 -0400

    last commit works with ansible. this commit is first attempt and making ssh-copy-id a function

--- python_readinventoryandcopysshkey.py
+++ python_readinventoryandcopysshkey.py
@@ -1,19 +1,18 @@
 from fun_parse_ini import parse_ini
 
-input_file = 'test.ini'
-sshpassword = ""
-sshpubkey = "/root/.ssh/id_rsa.pub"
-svcacc = "root"
+def password_auth_ssh_copy_id_from_array_input( input_file='/etc/ansible/hosts', svcacc = "root",
+                                                sshpassword = '' , sshpubkey = "/root/.ssh/id_rsa.pub" ):
+
+    #copy public ssh keys to systems in /etc/ansible/hosts
+    import subprocess
+    input_array = parse_ini(input_file)
+
+    for system in input_array:
+        # we define the bash command this was vs directly on subprocess because subprocess cannot handle
+        # white space inside quotes. 
+        bashcmd = 'sshpass -p %s ssh-copy-id -i %s %s@%s' % ( sshpassword, sshpubkey, svcacc, system )
+        # intiating the copy of ssh keys to systems in the inventory
+        subprocess.call(['bash','-c', bashcmd])
 
 
-#copy public ssh keys to systems in /etc/ansible/hosts
-import subprocess
-input_array = parse_ini(input_file)
-
-for system in input_array:
-    # we define the bash command this was vs directly on subprocess because subprocess cannot handle
-    # white space inside quotes. 
-    bashcmd = 'sshpass -p %s ssh-copy-id -i %s %s@%s' % ( sshpassword, sshpubkey, svcacc, system )
-
-    # intiating the copy of ssh keys to systems in the inventory
-    subprocess.call(['bash','-c', bashcmd])
+password_auth_ssh_copy_id_from_array_input()
