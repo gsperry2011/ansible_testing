033047d08a031118a91e1194c978367b320a1671
Greg Perry <gsperry2011@gmail.com>
2017-08-16 18:35:37 -0400

    tested and working. function takes 1 argument which should be an ini file. it will return a list of all entries within the ini file, removing headers, comments, and whitespace.

--- fun_parse_ini.py
+++ fun_parse_ini.py
@@ -4,24 +4,38 @@ def parse_ini(filename):
     f = open(filename, 'r')
     
     content = f.readlines()
-    output = []
-    output2 = []
-    
+
+    #what we will store our final output in
+    ini_file_output = []
+
     for line in content:
-        
+
         #filter out commented lines and group headers from input file
         if not (line.lstrip().startswith("#") or line.lstrip().startswith("[")):
-            output.append(line)
+            array_including_newlines = []
+            array_including_newlines.append(line)
+
+
+
+
             
             #removing \n characters from input
-            for object in output:
+            for object in array_including_newlines:
+                array_including_blank_entries = []
                 newlines = object.split('\n')
                 #newlines[0] is to select the string from the list that returns from the split. The split
                 #is (data, blank) or (blank, blank) which is dealt which below
-                output2.append(newlines[0])
+                array_including_blank_entries.append(newlines[0])
                 
                 #removes all blank entries from final list (these are left from removing new lines above)
-                output2 = filter(None, output2)
-            
-                #at this point output2 is a list of hostnames or ip addresses and ready for use.
-                return output2
+                array_including_blank_entries = filter(None, array_including_blank_entries)
+                
+                #at this point array_including_blank_entries is a list of hostnames or ip addresses and ready for use.
+
+
+                for system in array_including_blank_entries:
+
+                    ini_file_output.append(system)
+
+
+    return ini_file_output
