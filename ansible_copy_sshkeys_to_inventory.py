237885e000bdeb8342bde841166733ec991c1a44
Greg Perry <gsperry2011@gmail.com>
2017-08-16 19:31:50 -0400

    missed a ,

--- ansible_copy_sshkeys_to_inventory.py
+++ ansible_copy_sshkeys_to_inventory.py
@@ -3,4 +3,4 @@
 
 from fun_password_auth_ssh_copy_id_from_array_input import password_auth_ssh_copy_id_from_array_input
 
-password_auth_ssh_copy_id_from_array_input(svcacc=(sys.argv[1]) sshpassword=(sys.argv[2]), sshpubkey='/root/.ssh/id_rsa.pub')
+password_auth_ssh_copy_id_from_array_input(svcacc=(sys.argv[1]), sshpassword=(sys.argv[2]), sshpubkey='/root/.ssh/id_rsa.pub')
